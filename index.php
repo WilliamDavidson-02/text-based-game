01ffea5aa6e7eff45c1f8feec378ff0bc40f3e32
William Davidson <wj.davidson@outlook.com>
2023-10-13 15:38:33 +0200

    navigation between rooms + saving previous commands + initial styling

--- index.php
+++ index.php
@@ -2,30 +2,66 @@
 
 session_start();
 
-$rooms = [
-    [
-        'description' => 'room1',
-        'exits' => ['north' => 1]
-    ],
-    [
-        'description' => 'room2',
-        'exits' => ['south' => 0]
-    ],
-];
+require_once __DIR__ . '/rooms.php';
 
 $commands = [
-    'movements' => ['north', 'west', 'south', 'east'],
+    'directions' => ['north', 'west', 'south', 'east'],
+    'room_interactions' => ['look', 'take', 'move']
 ];
+$errorMessage = null;
 
-$_SESSION['currentRoom'] = isset($_SESSION['currentRoom']) ? $_SESSION['currentRoom'] : 0;
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (in_array($_POST['command'], $commands['movements'])) {
-    }
+if (!isset($_SESSION['player'])) {
+    $_SESSION['player'] = [
+        'current_room' => 'forest_entrance',
+        'inventory' => [],
+        'prev_commands' => [],
+    ];
 }
 
-?>
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    [$actionType, $action] = explode(' ', $_POST['command']);
+    switch (strtolower($actionType)) {
+        case 'move':
+            if (array_key_exists(strtolower($action), $rooms[$_SESSION['player']['current_room']]['connection'])) {
+                $_SESSION['player']['current_room'] = $rooms[$_SESSION['player']['current_room']]['connection'][strtolower($action)];
+            }
+            break;
+        case 'look':
 
+            break;
+        default:
+            $errorMessage = "You are unable to $actionType.";
+    }
+
+    // save prev commands, - 6 to get the 5 latest in the array.
+    $_SESSION['player']['prev_commands'][] = $_POST['command'];
+
+    if (count($_SESSION['player']['prev_commands']) > 5) {
+        array_splice($_SESSION['player']['prev_commands'], count($_SESSION['player']['prev_commands']) - 6);
+    }
+    // preventing same action to run again if page is reloaded.
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit;
+}
+
+require_once __DIR__ . '/header.php';
+?>
+<?php if ($errorMessage !== null) : ?>
+    <div><?= $errorMessage; ?></div>
+<?php endif; ?>
+<div class="">
+    <div>
+        <h1><?= $_SESSION['player']['current_room']; ?></h1>
+        <div><?= $rooms[$_SESSION['player']['current_room']]['description']; ?></div>
+    </div>
+    <div>
+        <?php foreach ($_SESSION['player']['prev_commands'] as $command) : ?>
+            <div><?= $command; ?></div>
+        <?php endforeach; ?>
+    </div>
+</div>
 <form method="post">
     <input name="command" type="text">
 </form>
+
+<?php require_once __DIR__ . '/footer.php'; ?>
---
+++ rooms.php
@@ -0,0 +1,32 @@
+<?php
+
+$rooms = [
+    'forest_entrance' => [
+        'description' => 'You stand at the entrance of the forest. Towering trees loom overhead, their branches reaching for the sky. The air is cool and carries a faint scent of moss and earth. Look around before you begin you adventure.',
+        'actions' => [
+            'look' => ['You notice a glimmering object half-buried in the soft soil.'],
+            'take' => 'As you pick it up, a mysterious energy pulses through your veins, urging you north.'
+        ],
+        'item' => ['Shimmering object'],
+        'connection' => ['north' => 'mystical_clearing']
+    ],
+    'mystical_clearing' => [
+        'description' => 'A small, peaceful clearing surrounded by ancient trees. Sunlight filters through the leaves, casting dappled patterns on the forest floor. In the center, there\'s a sparkling pool fed by a gentle stream. You can hear the soothing sound of running water.',
+        'actions' => [
+            'look' => ['A delicate silver key hangs from a low branch, catching the light.', 'West of you there is something whispering, what could it be.'],
+            'take' => 'With the key in hand, you feel a sense of purpose.'
+        ],
+        'item' => ['Silver key'],
+        'connection' => ['west' => 'whispering_grove', 'south' => 'forest_entrance']
+    ],
+    'whispering_grove' => [
+        'description' => 'This grove feels alive with magic. The trees here have faces carved into their bark, and they seem to murmur secrets to each other. A soft, ethereal light bathes the area, creating an otherworldly atmosphere.',
+        'actions' => [
+            'look' => ['A stone pedestal at the center of the grove holds a mysterious, ornate box.'],
+            'take' => 'You take the ornate box, the box is locked.',
+            'use' => ['key' => 'This key dose not work on the ornate box']
+        ],
+        'item' => ['ornate box'],
+        'connection' => ['east' => 'mystical_clearing']
+    ],
+];
